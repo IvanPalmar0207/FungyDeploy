cfc2fa8a360a3c7e13a4254100a4b2ef09c86e3a
IvanPalmar07 <palmar.ivan0205@gmail.com>
2024-08-02 11:25:03 -0500

    WhiteNoise

--- Proyecto_fungy/settings.py
+++ Proyecto_fungy/settings.py
@@ -127,7 +127,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
