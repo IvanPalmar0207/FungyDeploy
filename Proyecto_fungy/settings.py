bd7ca2cee9b00d7a30655cc46bf168d6830e489f
IvanPalmar07 <palmar.ivan0205@gmail.com>
2024-08-02 10:55:55 -0500

    DataBase

--- Proyecto_fungy/settings.py
+++ Proyecto_fungy/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 import os
-from pathlib import Pathff
+from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
