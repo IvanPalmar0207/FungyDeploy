e4604d686af5caeeabd737f6b59dc916f69f3eed
IvanPalmar07 <palmar.ivan0205@gmail.com>
2024-08-02 11:54:49 -0500

    DataBase PsotgreSQL

--- Proyecto_fungy/settings.py
+++ Proyecto_fungy/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 import os
 from pathlib import Path
+import dj_database_url
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -88,10 +92,7 @@ WSGI_APPLICATION = 'Proyecto_fungy.wsgi.application'
 
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',                
-    }
+    'default': dj_database_url.config(default = os.getenv('DATABASE_URL'))
 }
 
 # Password validation
