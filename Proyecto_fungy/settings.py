419b399214edb08eb366036c7887e699c4489ed8
Ivan Palmar <palmar.ivan0205@gmail.com>
2024-08-08 00:08:15 -0500

    Responsive - PartNro1

--- Proyecto_fungy/settings.py
+++ Proyecto_fungy/settings.py
@@ -97,11 +97,11 @@ WSGI_APPLICATION = 'Proyecto_fungy.wsgi.application'
 
 
 DATABASES = {
-    #'default': dj_database_url.config(default = os.getenv('DATABASE_URL'))
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default = os.getenv('DATABASE_URL'))
+    #'default': {
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': BASE_DIR / 'db.sqlite3',
+    #}
 }
 
 # Password validation
